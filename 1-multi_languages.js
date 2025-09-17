d77d90403e8e8ae38565a006f6521bbf9fc250a3
me <lorraobare@gmail.com>
2025-09-17 17:02:38 +0300

    Created second file

---
+++ 1-multi_languages.js
@@ -0,0 +1,16 @@
+/*Write a script that prints 3 lines:
+
+The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+You must use console.log(...) to print all output
+You are not allowed to use var
+ */
+
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";  
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
