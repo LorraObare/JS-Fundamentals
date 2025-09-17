b5d71f1ff85bd1aece47d2f1dfb61e2d41f25aa2
me <lorraobare@gmail.com>
2025-09-17 17:40:33 +0300

    factorial

---
+++ 10-factorial.js
@@ -0,0 +1,19 @@
+/*Write a script that computes and prints a factorial
+
+The first argument is integer (argument can be cast as integer) used for computing the factorial
+Factorial of NaN is 1
+You must do it recursively
+You must use a function
+You must use console.log(...) to print all output
+You are not allowed to use var*/
+
+function factorial(n) {
+    if (n <= 1 || isNaN(n)) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+const arg = parseInt(process.argv[2], 10);
+console.log(factorial(arg));
