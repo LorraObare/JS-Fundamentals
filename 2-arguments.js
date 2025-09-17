b2f4f4013f3607bb10233378e617cabad2b71248
me <lorraobare@gmail.com>
2025-09-17 17:05:38 +0300

    created 3rd file

---
+++ 2-arguments.js
@@ -0,0 +1,15 @@
+/*Write a script that prints a message depending of the number of arguments passed:
+
+If no arguments are passed to the script, print “No argument”
+If only one argument is passed to the script, print “Argument found”
+Otherwise, print “Arguments found”
+You must use console.log(...) to print all output
+You are not allowed to use var*/
+
+const argsCount = process.argv.length - 2;
+
+if (argsCount === 0) { 
+    console.log("No argument");
+} else if (argsCount === 1) {
+    console.log("Arguments found");
+}
